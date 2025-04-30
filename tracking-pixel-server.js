59dd3307461fcfe0e4c9c0f78ecff941e72bd83e
Mohd Shahzaib Hussain <shahzaib.hussain@optimiser.com>
2025-04-30 15:52:18 +0530

    Initial commit: Tracking Pixel Server

--- tracking-pixel-server.js
+++ tracking-pixel-server.js
@@ -7,11 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.set('trust proxy', true);
 
 app.get('/pixel.png', async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
   const userAgent = req.headers['user-agent'];
   const timestamp = new Date().toISOString();
-
-  console.log(req)
 
   let location = {};
 
